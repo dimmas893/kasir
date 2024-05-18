9204182aab63ef5a5124fedd38e2f88f5de32fc4
sebelum cahaya <telkominfraapps@gmail.com>
2024-05-19 06:40:30 +0700

    perbaikan bug

--- resources/views/laporan/view.blade.php
+++ resources/views/laporan/view.blade.php
@@ -3,55 +3,59 @@
 @section('title', ' - Detail Laporan')
 
 @section('content')
-<section class="section">
-    <div class="section-header">
-        <h1>Laporan</h1>
-    </div>
+    <section class="section">
+        <div class="section-header">
+            <h1>Laporan</h1>
+        </div>
 
-    <div class="section-body">
-        <div class="card shadow">
-            <div class="card-header bg-white">
-            <h4 class="text-primary">Detail Laporan</h4>
-            </div>
-            <div class="card-body">
-                <table class="table table-hover" id="table">
-                    <thead>
-                        <tr>
-                            <th>No.</th>
-                            <th>Kode transaksi</th>
-                            <th>Nama</th>
-                            <th>Harga</th>
-                            <th>Jumlah</th>
-                            <th>Diskon</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($data as $item)
-                        <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$item->kode_transaksi}}</td>
-                            <td>{{$item->barang}}</td>
-                            <td>{{$item->formatRupiah('harga')}}</td>
-                            <td>{{$item->jumlah}}</td>
-                            <td>{{$item->diskon}}%</td>
-                            <td>{{$item->formatRupiah('total')}}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <div class="card-footer bg-white">
-                <a href="{{ url(auth()->user()->level . '/laporan') }}" class="btn btn-sm btn-outline-warning"><i class="fas fa-caret-left"></i> Kembali</a>
+        <div class="section-body">
+            <div class="card shadow">
+                <div class="card-header bg-white">
+                    <h4 class="text-primary">Detail Laporan</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+
+                        <table class="table table-hover" id="table">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Kode transaksi</th>
+                                    <th>Nama</th>
+                                    <th>Harga</th>
+                                    <th>Jumlah</th>
+                                    <th>Diskon</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($data as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->kode_transaksi }}</td>
+                                        <td>{{ $item->barang }}</td>
+                                        <td>{{ $item->formatRupiah('harga') }}</td>
+                                        <td>{{ $item->jumlah }}</td>
+                                        <td>{{ $item->diskon }}%</td>
+                                        <td>{{ $item->formatRupiah('total') }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="card-footer bg-white">
+                    <a href="{{ url(auth()->user()->level . '/laporan') }}" class="btn btn-sm btn-outline-warning"><i
+                            class="fas fa-caret-left"></i> Kembali</a>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 @push('script')
-<script>
-    $(document).ready(function () {
-        $('#table').DataTable();
-    });
-</script>
+    <script>
+        $(document).ready(function() {
+            $('#table').DataTable();
+        });
+    </script>
 @endpush
